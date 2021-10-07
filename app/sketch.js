4b44d54f7e802bcefe0666655ff80185ccf1538a
Jake Howell <jake.howell302@gmail.com>
2021-10-07 00:58:48 -0500

    yeet

--- app/sketch.js
+++ app/sketch.js
@@ -1,10 +1,10 @@
-function setup() { 
-    createCanvas(1000, 600); 
+function setup() { 
+    createCanvas(1000, 600); 
     background(220);
 }
 
-function draw() {  
-    ellipse(mouseX, mouseY, mouseY / 3)  
-    let c = color(mouseX, mouseY, Math.floor(Math.random() * 1000)) 
+function draw() {  
+    ellipse(mouseX, mouseY, mouseY / 3)  
+    let c = color(mouseX, mouseY, Math.floor(Math.random() * 1000)) 
     fill(c)
 }
