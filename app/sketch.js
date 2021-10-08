379cdbbc67931d7b6d791bfc6d0f37b4a52fbf2c
Jake Howell <jakehowellphoto@gmail.com>
2021-10-08 00:44:43 -0500

    change pencil by clicking mouse

--- app/sketch.js
+++ app/sketch.js
@@ -1,3 +1,5 @@
+let shapeSwitch = 0;
+
 function setup() { 
     createCanvas(windowWidth, windowHeight); 
     background(51); //TODO@lavalord changed the background color
@@ -5,9 +7,30 @@ function setup() { 
 }
 
 function draw() {
-    triangle(mouseX, mouseX*2, mouseY*2, mouseX,0,0)
+  if(shapeSwitch === 0)
+    {
+    ellipse(mouseX, mouseY, mouseY / 3)
+    }
+  if(shapeSwitch === 1)
+    {
+      triangle(mouseX, mouseX*2, mouseY*2, mouseX,0,0)
+    }
+  if(shapeSwitch === 2)
+    {
+      square(mouseX, mouseX * -1, mouseY);
+    }
+    
   let c = color(mouseX*2, mouseY/6, Math.floor(Math.random() * 777)) 
   fill(c)
+}
+
+function mousePressed() {
+  shapeSwitch++;
+  
+  if(shapeSwitch === 3)
+    {
+      shapeSwitch = 0;
+    }
 }
 
 function windowResized() {
